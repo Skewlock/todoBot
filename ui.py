85c12d35450bd6bf7ad04bd0f9c6c0b1df001f64
Skewlock <Skewlock@gmail.com>
2026-01-11 19:34:49 +0100

    persitent views test 3

--- ui.py
+++ ui.py
@@ -4,10 +4,10 @@ class TaskView(discord.ui.View):
     def __init__(self):
         super().__init__(timeout=None)
     
-    @discord.ui.button(style=discord.ButtonStyle.green, label="Task done !")
+    @discord.ui.button(style=discord.ButtonStyle.green, label="Task done !", custom_id="todo:check")
     async def taskDone(self, interaction: discord.Interaction, button: discord.ui.Button):
         print("AAAAAAAAAAAAAAAAAAAAAAAA")
 
-    @discord.ui.button(style=discord.ButtonStyle.red, label="Task canceled !")
+    @discord.ui.button(style=discord.ButtonStyle.red, label="Task canceled !", custom_id="todo:failed")
     async def taskFailed(self, interaction: discord.Interaction, Button: discord.ui.Button):
         print("BBBBBBBBBBBBBBBBBBBBBBBBBB")
