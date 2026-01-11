a773b085d23a0b71b9893203b013ab980502986b
Skewlock <Skewlock@gmail.com>
2026-01-11 16:28:25 +0100

    fixed ping embed

--- commands.py
+++ commands.py
@@ -4,7 +4,7 @@ async def ping(msg, bot):
     time_then = time.monotonic()
     pinger = await msg.channel.send("Calcul en cours du ping...")
     ping_ = (1000 * (time.monotonic() - time_then))
-    embed = discord.Embed(name="Results :", color=0x4c99c2, type="rich")
+    embed = discord.Embed(title="Results :", color=0x4c99c2, type="rich")
     embed.add_field(name="Ping du bot :", value=str(int(ping_)) + "ms", inline=True)
     embed.add_field(name="Ping de l'API :", value=str(int(bot.latency * 1000)) + "ms", inline=True)
     await pinger.edit(content=":white_check_mark: Calcul terminé !", embed=embed)
