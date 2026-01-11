d8d28217d8c228b24fa6c586b0d85041a316a313
Skewlock <Skewlock@gmail.com>
2026-01-11 18:52:13 +0100

    create task view test 2

--- ui.py
+++ ui.py
@@ -4,10 +4,10 @@ class TaskView(discord.ui.View):
     def __init__(self):
         super().__init__()
     
-    @discord.ui.Button(style=discord.ButtonStyle.green, label="Task done !")
+    @discord.ui.button(style=discord.ButtonStyle.green, label="Task done !")
     async def taskDone(interaction: discord.Interaction, button: discord.ui.Button):
         pass
 
-    @discord.ui.Button(style=discord.ButtonStyle.red, label="Task canceled !")
+    @discord.ui.button(style=discord.ButtonStyle.red, label="Task canceled !")
     async def taskFailed(interaction: discord.Interaction, Button: discord.ui.Button):
         pass
