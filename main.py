0fe3e17568551c838342a01a3386f18256cac845
Skewlock <Skewlock@gmail.com>
2026-01-11 16:19:35 +0100

    main bot starting

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import discord, os
 
-bot = discord.Client()
+botIntents = discord.Intents.all()
+
+bot = discord.Client(botIntents)
 bot.prefix = "!"
 
 print("Bot started:\n\n")
@@ -8,48 +10,34 @@ print("Bot started:\n\n")
 @bot.event
 async def on_message(msg):
     # Get command
-    if msg.author != bot.user:
-        if msg.content[0] == bot.prefix:
-            instruction = msg.content[1:]
-            command = instruction.split(" ")[0]
-            logs.printInfo("Command "+command+" requested.")
-            commands = {
-                #General commands
-                "ping": [cmds.ping, (msg, bot)],
-                "purge": [cmds.purge, (msg,)],
-                "help": [cmds.help, (msg,)],
-                "sinit": [db.init_server, (msg,)],
-                "module": [cmds.module, (msg,)],
-                "profile": [cmds.profile, (msg,)],
+    if msg.author == bot.user:
+        return
 
-                #Fun commands
-                "fact": [fun.fact, (msg,)],
+    if msg.content[0] != bot.prefix:
+        return
 
-                #Currency commands
-                "init": [db.init_user, (msg,)],
-                "balance": [curr.balance, (msg,)],
-                "daily": [curr.daily, (msg,)],
+    instruction = msg.content[1:]
+    command = instruction.split(" ")[0]
+    logs.printInfo("Command " + command + " requested.")
 
-                #NSFW commands
-                "rule34": [nsfw.rule34, (msg,)],
-                #"porn": [nsfw.porn, (msg,)]
-            }
-            # Launch command
-            func = commands.get(command)
-            if func is None:
-                return logs.printWarning("Command "+command+" doesn't exists.\n")
-            await func[0](*func[1])
-            logs.printInfo("Command "+command+" successfully done.\n")
+    commands = {
+        #General commands
+        "ping": [cmds.ping, (msg, bot)]
+    }
+
+    # Launch command
+    func = commands.get(command)
+    if func is None:
+        return logs.printWarning("Command " + command + " doesn't exists.\n")
+    await func[0](*func[1])
+    print("Command " + command + " successfully done.\n")
 
 
 @bot.event
 async def on_ready():
-    await db.init()
     logs.printValid("Bot ready !")
     logs.printValid("Default prefix: " + bot.prefix)
-    logs.printValid("Mention: "+bot.user.mention+"\n")
-    await bot.change_presence(activity=discord.Streaming(name="Prefix: !",
-    plateform="Twitch", url="https://www.youtube.com/watch?v=dQw4w9WgXcQ", game="test"))
-
+    logs.printValid("Mention: " + bot.user.mention + "\n")
+    await bot.change_presence(status=discord.Status.online, activity=discord.Game("prefix: "+ bot.prefix))
 
 bot.run(os.environ["TOKEN"])
