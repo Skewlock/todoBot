197691d14f5a5c205f26ba6f5011744d1b204b84
Skewlock <Skewlock@gmail.com>
2026-01-11 17:19:53 +0100

    refacto + slash command test 7

--- main.py
+++ main.py
@@ -1,53 +1,71 @@
 import discord, os
+from discord.ext import commands
 from discord import app_commands
 import logs
-import commands as cmds
+
+GUILD_ID = discord.Object(id=1459887017470201939)
+
+class Client(commands.Bot):
+
+    async def on_ready():
+        logs.printValid("Bot ready !")
+        logs.printValid("Default prefix: " + self.command_prefix)
+        logs.printValid("Mention: " + self.user.mention + "\n")
+        await bot.change_presence(status=discord.Status.online, activity=discord.Game("prefix: "+ bot.prefix))
+        try:
+            synced = await tree.sync(guild=GUILD_ID)
+            print(f'Synced {len(synced)} commands to guild {GUILD_ID.id}')
+        except Exception as e:
+            print("Error syncing command: " + e)
+
+
+    async def on_message(msg):
+        # Get command
+        if msg.author == self.user:
+            return
+
+        if msg.content[0] != self.prefix:
+            return
+
+        instruction = msg.content[1:]
+        command = instruction.split(" ")[0]
+        logs.printInfo("Command " + command + " requested.")
+        commands = {
+            # General commands
+            "ping": [cmds.ping, (msg, bot)]
+        }
+        # Launch command
+        func = commands.get(command)
+        if func is None:
+            return logs.printWarning("Command " + command + " doesn't exists.\n")
+        await func[0](*func[1])
+        logs.printInfo("Command " + command + " successfully done.\n")
+
 
 botIntents = discord.Intents.all()
+bot = Client(command_prefix="!", intents=botIntents)
 
-bot = discord.Client(intents=botIntents)
-bot.prefix = "!"
-
-tree = app_commands.CommandTree(bot)
-
-@tree.command(
-    name="create_task",
-    description="Une command pour créer un tâche dans la todo list",
-    guild=discord.Object(id=1459887017470201939)
+@bot.tree.command(
+    name="ping",
+    description="[DEBUG COMMAND] pong",
+    guild=GUILD_ID
 )
+async def ping(interaction: discord.Interaction):
+    time_then = time.monotonic()
+    pinger = await interaction.response.send_message("Calcul en cours du ping...")
+    ping_ = (1000 * (time.monotonic() - time_then))
+    embed = discord.Embed(title="Results :", color=0x4c99c2, type="rich")
+    embed.add_field(name="Ping du bot :", value=str(int(ping_)) + "ms", inline=True)
+    embed.add_field(name="Ping de l'API :", value=str(int(bot.latency * 1000)) + "ms", inline=True)
+    await pinger.edit_message(content=":white_check_mark: Calcul terminé !", embed=embed)
 
-@bot.event
-async def on_message(msg):
-    # Get command
-    if msg.author == bot.user:
-        return
+@bot.tree.command(
+    name="create_task",
+    description="Crée une tâche dans la todo list",
+    guild=GUILD_ID
+)
+async def create_task(interaction: discord.Interaction, task_name: str):
+    await interaction.response.send_message("test")
 
-    if msg.content[0] != bot.prefix:
-        return
-
-    instruction = msg.content[1:]
-    command = instruction.split(" ")[0]
-    logs.printInfo("Command " + command + " requested.")
-
-    commands = {
-        # General commands
-        "ping": [cmds.ping, (msg, bot)]
-    }
-
-    # Launch command
-    func = commands.get(command)
-    if func is None:
-        return logs.printWarning("Command " + command + " doesn't exists.\n")
-    await func[0](*func[1])
-    logs.printInfo("Command " + command + " successfully done.\n")
-
-
-@bot.event
-async def on_ready():
-    logs.printValid("Bot ready !")
-    logs.printValid("Default prefix: " + bot.prefix)
-    logs.printValid("Mention: " + bot.user.mention + "\n")
-    await tree.sync(guild=discord.Object(id=1459887017470201939))
-    await bot.change_presence(status=discord.Status.online, activity=discord.Game("prefix: "+ bot.prefix))
 
 bot.run(os.environ["TOKEN"])
