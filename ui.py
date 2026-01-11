4336c58cff9f9997c527d4d514a18cd6f507e0ed
Skewlock <Skewlock@gmail.com>
2026-01-11 19:44:29 +0100

    persitent views test 5

--- ui.py
+++ ui.py
@@ -4,12 +4,14 @@ class TaskView(discord.ui.View):
     def __init__(self):
         super().__init__(timeout=None)
     
-    @discord.ui.button(style=discord.ButtonStyle.green, label="Task done !", custom_id="todo:check")
+    @discord.ui.button(style=discord.ButtonStyle.green, label="Finish task", custom_id="todo:check")
     async def taskDone(self, interaction: discord.Interaction, button: discord.ui.Button):
-        print("AAAAAAAAAAAAAAAAAAAAAAAA")
         await interaction.response.send_message('Task marked as success !', ephemeral=True)
 
-    @discord.ui.button(style=discord.ButtonStyle.red, label="Task canceled !", custom_id="todo:failed")
+    @discord.ui.button(style=discord.ButtonStyle.blurple, label="Repoen task", custom_id="todo:reopened")
+    async def taskReopened(self, interaction: discord.Interaction, Button: discord.ui.Button):
+        await interaction.response.send_message('Task reopened', ephemeral=True)
+
+    @discord.ui.button(style=discord.ButtonStyle.red, label="Cancel task", custom_id="todo:failed")
     async def taskFailed(self, interaction: discord.Interaction, Button: discord.ui.Button):
-        print("BBBBBBBBBBBBBBBBBBBBBBBBBB")
         await interaction.response.send_message('Task marked as failed', ephemeral=True)
