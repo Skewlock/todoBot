38e49c1f9f38edc655c21ec239c952c9acdf1187
Skewlock <Skewlock@gmail.com>
2026-01-12 09:13:56 +0100

    embed editting test 5

--- ui.py
+++ ui.py
@@ -9,28 +9,31 @@ class TaskView(discord.ui.View):
         embed_title = interaction.message.embeds[0].title
         created = interaction.message.embeds[0].fields[0].value
         mention = interaction.message.mentions[0].mention
+        msg_content = "New task for " + mention + ": " + embed_title
 
         embed = discord.Embed(title=embed_title, color=discord.Colour.green(), type='rich')
         embed.add_field(name="Created:", value=created)
-        await interaction.message.edit("New task for " + mention + ": " + embed_title, view=TaskView(), embed=embed)
+        await interaction.message.edit(content=msg_content, view=TaskView(), embed=embed)
         await interaction.response.send_message('Task marked as success !', ephemeral=True)
 
     @discord.ui.button(style=discord.ButtonStyle.blurple, label="Repoen task", custom_id="todo:reopened")
     async def taskReopened(self, interaction: discord.Interaction, Button: discord.ui.Button):
         embed_title = interaction.message.embeds[0].title
         created = interaction.message.embeds[0].fields[0].value
+        msg_content = "New task for " + mention + ": " + embed_title
 
         embed = discord.Embed(title=embed_title, color=discord.Colour.blurple(), type='rich')
         embed.add_field(name="Created:", value=created)
-        await interaction.message.edit("New task for " + mention + ": " + embed_title, view=TaskView(), embed=embed)
+        await interaction.message.edit(content=msg_content, view=TaskView(), embed=embed)
         await interaction.response.send_message('Task reopened', ephemeral=True)
 
     @discord.ui.button(style=discord.ButtonStyle.red, label="Cancel task", custom_id="todo:failed")
     async def taskFailed(self, interaction: discord.Interaction, Button: discord.ui.Button):
         embed_title = interaction.message.embeds[0].title
         created = interaction.message.embeds[0].fields[0].value
+        msg_content = "New task for " + mention + ": " + embed_title
 
         embed = discord.Embed(title=embed_title, color=discord.Colour.red(), type='rich')
         embed.add_field(name="Created:", value=created)
-        await interaction.message.edit("New task for " + mention + ": " + embed_title, view=TaskView(), embed=embed)
+        await interaction.message.edit(content=msg_content, view=TaskView(), embed=embed)
         await interaction.response.send_message('Task marked as failed', ephemeral=True)
